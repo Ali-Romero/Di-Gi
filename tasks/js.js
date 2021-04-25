52ec867548a5d5a1098cd626d59addd6c901190e
17axaH <17axaH@mail.ru>
2021-04-25 18:16:57 +0000

    compress js

--- tasks/js.js
+++ tasks/js.js
@@ -1,5 +1,8 @@
 const { src, dest } = require('gulp')
+const gulpif = require('gulp-if')
 const include = require('gulp-include')
+const uglify = require('gulp-uglify')
+const rename = require('gulp-rename')
 const path = require('path')
 
 const config = {
@@ -9,8 +12,12 @@ const config = {
   ]
 }
 
+const production = process.env.NODE_ENV === 'production'
+
 module.exports = function() {
   return src('source/js/*.js')
     .pipe(include(config))
+    .pipe(gulpif(production, uglify()))
+    .pipe(gulpif(production, rename({ suffix: '.min' })))
     .pipe(dest('dest/js'))
 }
