71980cecf6c2eb1cd37785c7cbdd0f967646bc60
17axaH <17axaH@mail.ru>
2021-04-25 18:03:39 +0000

    prod css min

--- tasks/sass.js
+++ tasks/sass.js
@@ -1,4 +1,7 @@
 const { src, dest } = require('gulp')
+const gulpif = require('gulp-if')
+const cssmin = require('gulp-cssmin')
+const rename = require('gulp-rename')
 const sass = require('gulp-sass')
 
 sass.compiler = require('node-sass')
@@ -7,8 +10,12 @@ const config = {
   includePaths: ['node_modules']
 }
 
+const production = process.env.NODE_ENV === 'production'
+
 module.exports = function() {
   return src('source/sass/*.sass')
     .pipe(sass(config).on('error', sass.logError))
+    .pipe(gulpif(production, cssmin()))
+    .pipe(gulpif(production, rename({ suffix: '.min' })))
     .pipe(dest('dest/css'))
 }
