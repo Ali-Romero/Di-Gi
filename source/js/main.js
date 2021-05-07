211796470537a0d941721651db75ed087ad85e14
Sam <sherali920825@yandex.ru>
2021-05-08 02:05:05 +0600

    continue to work

--- source/js/main.js
+++ source/js/main.js
@@ -2,13 +2,47 @@ $(document).ready(function(){
    $('.js-slider').slick({
       dots: true,
       arrows: true,
-      infinite:false,
+      infinite: false,
+      initialSlide:1,
+      appendArrows: $('.main-slider__prev'),
+      prevArrow: $('.main-slider__prev'),
+      nextArrow: $('.main-slider__next'),
+      appendDots: $('.main-slider__dots'),
    })
 });
 
-// $('.arrow-prew').click(function(event) {
-//    $('.js-slider').slick('slickPrev');
+// $(".main-slider__next").click(function(){ 
+//     $(".main-slider__next").css("background-color","#F9F6E5");
 // });
-// $('.arrow-next').click(function(event) {
-//    $('.js-slider').slick('slickNext');
-// })
+
+// $('img.content_item').each(function(){
+//   var $img = $(this);
+//   var imgClass = $img.attr('class');
+//   var imgURL = $img.attr('src');
+//   $.get(imgURL, function(data) {
+//     var $svg = $(data).find('svg');
+//     if(typeof imgClass !== 'undefined') {
+//       $svg = $svg.attr('class', imgClass+' replaced-svg');
+//     }
+//     $svg = $svg.removeAttr('xmlns:a');
+//     if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+//       $svg.attr('viewBox', '0 0 16 26' + $svg.attr('height') + ' ' + $svg.attr('width'))
+//     }
+//     $img.replaceWith($svg);
+//   }, 'xml');
+// });
+
+// $('.main-slider__next').on("click", function() {
+//     $('.test').css({ fill: "#929292" });
+// });
+// // $('.arrow-prew').click(function(event) {
+// //    $('.js-slider').slick('slickPrev');
+// // });
+// // $('.arrow-next').click(function(event) {
+// //    $('.js-slider').slick('slickNext');
+// // })
+
+$('.js-trigger').click(function () {
+  $(this).next().slideToggle(320)
+  $(this).toggleClass('active')
+});
